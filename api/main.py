e0d9bdeb218c3460c677a09a7a1ff873f4e2004b
MarzaElise <anfasalma2007@gmail.com>
2021-06-23 11:46:52 +0530

    facts endpoint working now

--- api/main.py
+++ api/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI, Request, Response
-# from starlette.responses import Response
-from Helpers import random_facts
-from Helpers import website as web
+import traceback
+import asyncio
+from discord.ext import tasks
+from helpers import website as web
+from helpers import random_facts
+# from ..helpers import website as web
+# from ..helpers import random_facts
 from fastapi.responses import RedirectResponse
 import aiohttp
 import random
@@ -13,40 +17,17 @@ limiter = Limiter(key_func=get_remote_address, headers_enabled=True)
 app = FastAPI(debug=True, title="randomness", description="An api you can use to generate random facts and websites", version="1.0.0a")
 app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+# web = helpers.website
+# random_facts = helpers.random_facts
 
-async def fetch_text():
-    async with aiohttp.ClientSession() as ses:
-        async with ses.get("https://uselessfacts.jsph.pl/random.json?language=en") as res:
-            data = await res.json()
-            text = data["text"]
-            return text
-
-async def fetch_fact():
-    async with aiohttp.ClientSession() as ses:
-        async with ses.get("https://some-random-api.ml/facts/cat") as res:
-            data = await res.json()
-            try:
-                fact = data["fact"]
-            except KeyError:
-                return None
-            else:
-                return fact
-
-async def get_lis():
-    lis = []
-    for i in range(30):
-        try:
-            text = await fetch_text()
-        except:
-            continue
-        else:
-            lis.append(text)
-    for i in range(40):
-        fact = await fetch_fact()
-        if fact:
-            lis.append(fact)
-    random_facts.extend(lis)
-    return random_facts
+@app.on_event("startup")
+async def on_startup():
+    '''Startup event that is triggered when the app is starting to run'''
+    extend_facts_useless.start()
+    extend_facts_some.start()
+    dog_fact.start()
+    panda.start()
+    print("Server Started Succesfully")
 
 @app.get("/")
 async def home():
@@ -57,12 +38,11 @@ async def home():
 # @limiter.limit("3/second")
 async def fact():
     '''Generate a random fact. More facts will be added very soon'''
-    return {"fact" : "Fun Fact: the fact endpoint is dead until i find a way to not get rate limitted"}
-    # facts = await get_lis()
-    # fact = random.choice(facts)
-    # index = facts.index(fact)
-    # total = len(random_facts) - 1
-    # return {"fact" : fact, "index" : index, "total" : total}
+    # return {"fact" : "Fun Fact: the fact endpoint is dead until i find a way to not get rate limitted"}
+    fact = random.choice(random_facts)
+    index = random_facts.index(fact)
+    total = len(random_facts) - 1
+    return {"fact" : fact, "index" : index, "total" : total}
 
 @app.get("/website")
 @limiter.limit("3/minute")
@@ -72,3 +52,80 @@ async def website(request : Request, response : Response):
     a = web.index(e)
     total = len(web) - 1
     return {"website" : e, "index" : a, "total" : total}
+
+
+@tasks.loop(minutes=1)
+async def extend_facts_useless():
+    '''Get some facts and append it to the random_facts list'''
+    async with aiohttp.ClientSession() as ses:
+        async with ses.get("https://uselessfacts.jsph.pl/random.json?language=en") as res:
+            try:
+                await res.json()
+            except Exception as e:
+                # traceback.print_exception(type(e), e, e.__traceback__)
+                await asyncio.sleep(60 * 10)
+            else:
+                data = await res.json()
+                text = data["text"]
+                random_facts.append(text)
+                print("Appended to list - useless")
+                await ses.close()
+                # extend_facts_some.start()
+    return None
+
+@tasks.loop(minutes=1)
+async def extend_facts_some():
+    '''Extend the random_facts list using some-random-api cat endpoint'''
+    async with aiohttp.ClientSession() as ses:
+        async with ses.get("https://some-random-api.ml/facts/cat") as res:
+            try:
+                await res.json()
+            except Exception as e:
+                # traceback.print_exception(type(e), e, e.__traceback__)
+                await asyncio.sleep(60 * 10)
+            else:
+                data = await res.json()
+                _fact = data["fact"]
+                random_facts.append(_fact)
+                print("Appended to list - cat")
+                await ses.close()
+                # dog_fact.start()
+    return None
+
+@tasks.loop(minutes=1)
+async def dog_fact():
+    '''Dog facts from some-random-api.ml dog endpoint'''
+    async with aiohttp.ClientSession() as ses:
+        async with ses.get("https://some-random-api.ml/facts/dog") as res:
+            try:
+                await res.json()
+            except Exception as e:
+                # traceback.print_exception(type(e), e, e.__traceback__)
+                await asyncio.sleep(60 * 10)
+            else:
+                data = await res.json()
+                dog = data["fact"]
+                random_facts.append(dog)
+                print("Appended to list - dog")
+                await ses.close()
+                # panda.start()
+    return None
+
+@tasks.loop(minutes=1)
+async def panda():
+    '''https://some-random-api.ml/facts Panda endpoint'''
+    async with aiohttp.ClientSession() as ses:
+        async with ses.get("https://some-random-api.ml/facts/panda") as res:
+            try:
+                await res.json()
+            except Exception as e:
+                # traceback.print_exception(type(e), e, e.__traceback__)
+                await asyncio.sleep(60 * 10)
+            else:
+                data = await res.json()
+                _fact = data["fact"]
+                random_facts.append(_fact)
+                print("Appended to list - panda")
+                await ses.close()
+                # extend_facts_useless.start()
+    return None
